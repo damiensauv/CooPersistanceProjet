e3b78d64b2c58f3f13456acbd1ae4e2d7698e96c
damien sauvalle <damiensauvalle@gmail.com>
2017-11-16 15:05:03 +0100

    add ojdbc6 .... bad method, view why with not working with Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import Oracle.Oracle;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("Oracle.Oracle");
+        Connection c = Oracle.getConnection("SAUVALLE", "CooProject");
+        c.setAutoCommit(false);
+
+
+
     }
 
 }
---
+++ src/main/java/Oracle/Oracle.java
@@ -0,0 +1,21 @@
+package Oracle;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Oracle {
+    private static Connection connection;
+
+    public static Connection getConnection(String username, String password) throws SQLException, SQLException {
+        if (connection == null) {
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora", username, password);
+            connection.setAutoCommit(false);
+            return connection;
+        } else {
+            return connection;
+        }
+    }
+
+}
