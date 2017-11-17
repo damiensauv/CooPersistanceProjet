6c9a023238990d2c6ba14c651727f04a09c5bd81
damien sauvalle <damiensauvalle@gmail.com>
2017-11-17 17:25:47 +0100

    Feat : DataMapper & PersonneMapper basic

--- src/main/java/Persistance/Mapper/DataMapper.java
+++ src/main/java/Persistance/Mapper/DataMapper.java
@@ -1,4 +1,14 @@
 package Persistance.Mapper;
 
-public class DataMapper {
+public abstract class DataMapper<T> {
+
+    public DataMapper() {}
+
+    abstract T find(T o);
+
+    abstract void insert(T o);
+
+    abstract void delete(T o);
+
+    abstract void update(T o);
 }
---
+++ src/main/java/Persistance/Mapper/PersonneMapper.java
@@ -0,0 +1,23 @@
+package Persistance.Mapper;
+
+import Entity.Personne;
+
+public class PersonneMapper extends DataMapper<Personne> {
+
+
+    Personne find(Personne o) {
+        return null;
+    }
+
+    void insert(Personne o) {
+
+    }
+
+    void delete(Personne o) {
+
+    }
+
+    void update(Personne o) {
+
+    }
+}
