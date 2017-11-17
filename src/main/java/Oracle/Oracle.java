1f708a5d7320aac4f645a0c8fa524682cd587483
damien sauvalle <damiensauvalle@gmail.com>
2017-11-17 17:03:16 +0100

    refactor : clean code

--- src/main/java/Oracle/Oracle.java
+++ src/main/java/Oracle/Oracle.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Oracle {
     private static Connection connection;
 
-    public static Connection getConnection(String username, String password) throws SQLException, SQLException {
+    public static Connection getConnection(String username, String password) throws SQLException {
         if (connection == null) {
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora", username, password);
             connection.setAutoCommit(false);
