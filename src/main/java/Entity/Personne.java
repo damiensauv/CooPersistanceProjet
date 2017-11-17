f37315b79c986ed833de137777ee37c2c417b65e
damien sauvalle <damiensauvalle@gmail.com>
2017-11-17 16:51:51 +0100

    feat : ajout Entity personne

---
+++ src/main/java/Entity/Personne.java
@@ -0,0 +1,44 @@
+package Entity;
+
+public class Personne {
+
+    private int i;
+    private String nom;
+    private String prenom;
+    private String evaluation;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(String evaluation) {
+        this.evaluation = evaluation;
+    }
+
+
+
+}
