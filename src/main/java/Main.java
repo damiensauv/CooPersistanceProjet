6547f0ba3f299f7620748aa6f6a5f229d3cb193a
damien sauvalle <damiensauvalle@gmail.com>
2017-11-19 18:19:42 +0100

    Refacor : make Connection Singleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
-import Oracle.Oracle;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("Oracle.Oracle");
-        Connection c = Oracle.getConnection("SAUVALLE", "CooProject");
-        c.setAutoCommit(false);
 
+//        Connection c = Oracle.getInstance();
 
 
     }
--- src/main/java/Oracle/Oracle.java
+++ src/main/java/Oracle/Oracle.java
@@ -1,21 +1,34 @@
 package Oracle;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Oracle {
+
     private static Connection connection;
 
-    public static Connection getConnection(String username, String password) throws SQLException {
-        if (connection == null) {
-            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora", username, password);
-            connection.setAutoCommit(false);
-            return connection;
-        } else {
-            return connection;
+    private Oracle() {
+        try {
+            initConnexion();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
+
+    private void initConnexion() throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.OracleDriver");
+        connection = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fil.univ-lille1.fr:1521:filora", "SAUVALLE", "CooProject");
+        connection.setAutoCommit(false);
+    }
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            new Oracle();
+        }
+        return connection;
     }
 
 }
